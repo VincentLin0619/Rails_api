73db52e5a96e9dc293e83de946b2acb7440f9ed0
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-11-24 16:00:34 +0800

    測試 article object 資料表驗證

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    title { "test title" }
+    content { "test content" }
+    sequence(:slug) { |n| "sample-article#{n}" }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,32 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#validations" do
+    let(:article) { FactoryBot.create(:article) }
+    it "test Article is valid?" do
+      expect(article).to be_valid
+    end
+
+    it "測試title是否驗證不能為空" do
+      article.title = ""
+      expect(article).not_to be_valid
+      # 期望錯誤訊息是 can't be blank
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+    it "測試content是否驗證不能為空" do
+      article.content = ""
+      expect(article).not_to be_valid
+      # 期望錯誤訊息是 can't be blank
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+    it "測試slug是否驗證為唯一值" do
+      article_1 = FactoryBot.create(:article)
+      expect(article_1).to be_valid
+      article_2 = FactoryBot.build(:article, slug: article_1.slug)
+      expect(article_2).not_to be_valid
+
+      # 期望錯誤訊息是 Slug has already been taken
+      expect(article_2.errors[:slug]).to include("has already been taken")
+    end
+  end
 end
